eae6f85f19505de6de29dc748e54a427ed0973b0
Sean T Dolan <sdolan@caltech.edu>
2015-02-08 18:03:00 -0800

    Process reduce should be done. Mostly untested.

--- data/process_reduce.py
+++ data/process_reduce.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python
 
 import sys
+import re
+import operator
 
 # TODO: HANDLE CASE WHERE DONE BEFORE 50th ITERATION
 
 
+get_score_string = "\d{9}\t\d{1,2},(\d*.\d*)"
+get_score_regex = re.compile(get_score_string)
+
+min_node = "-1"
 top_20 = {}
+
+
+def compute_min():
+    global min_node
+    min_pagerank = None
+    for node, pagerank in top_20.iteritems():
+        if min_pagerank is None:
+            min_node = node
+            min_pagerank = pagerank
+        elif pagerank < min_pagerank:
+            min_node = node
+            min_pagerank = pagerank
+
 
 for line in sys.stdin:
     # Get iterations.
@@ -16,8 +35,19 @@ for line in sys.stdin:
 
     # End computation.
     if iteration == 50:
-        pass
+        node = line[:9]
+        score = float(get_score_regex.match(line).group(1))
 
+        if len(top_20) < 20:
+            top_20[node] = score
+
+        else:
+            compute_min()
+            if top_20[min_node] < score:
+                del top_20[min_node]
+                top_20[node] = score
+
+    # Keep iterating.
     else:
         sys.stdout.write("NodeId:%s,%d\t" % (line[:9], iteration))
 
@@ -28,3 +58,10 @@ for line in sys.stdin:
 
         # rest_of_data already has newline.
         sys.stdout.write("%s" % rest_of_data)
+
+# If we're done.
+if top_20:
+    top_20_sorted = sorted(top_20.items(), key=operator.itemgetter(1))
+    top_20_sorted.reverse()
+    for node, pagerank in top_20_sorted:
+        sys.stdout.write("FinalRank:%f\t%s\n" % (pagerank, node))
